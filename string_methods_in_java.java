b4741e5796768c9d7c723ab5be27381d522e3f22
Sibasish784 <01sibasish@gmail.com>
2023-08-25 14:41:02 +0530

    code

--- string_methods_in_java.java
+++ string_methods_in_java.java
@@ -1,11 +1,10 @@
 import java.util.*;
 public class string_methods_in_java {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         String name = "S";
-=======
-        String name = "Sibasish";
->>>>>>> origin/main
+
+
         System.out.println(name);
         int name1 = name.length();
         System.out.println(name1);
