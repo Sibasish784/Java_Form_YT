4aea703b565fcb3bc9b2dbd8f75c5f711548209b
Sibasish784 <01sibasish@gmail.com>
2023-08-25 15:40:56 +0530

    code

--- string_methods_in_java.java
+++ string_methods_in_java.java
@@ -2,11 +2,15 @@ import java.util.*;
 public class string_methods_in_java {
     public static void main(String[] args) {
 
-        String name = "S";
-
-
+        String name = "SIBASISHsibasish";
         System.out.println(name);
         int name1 = name.length();
         System.out.println(name1);
+        String Ansu = name.toLowerCase();
+        System.out.println(Ansu);
+
+        String Ansu1 = name.toUpperCase();
+        System.out.println(Ansu1);
+        System.out.println(name.substring(8) );
     }
 }
