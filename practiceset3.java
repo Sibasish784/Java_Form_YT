34c0680abd6db8eb88f8e3272ddb214ce3e5b887
Sibasish784 <01sibasish@gmail.com>
2023-08-29 18:56:35 +0530

    code

---
+++ practiceset3.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+
+public class practiceset3 {
+    public static void main(String[] args) {
+       // q1
+//        String q1 = "SIBASISH";
+//        System.out.println("The String is "+ q1);
+//        System.out.println("After conviring to lower case the string is");
+//        String q1a = q1.toLowerCase();
+//        System.out.println(q1a);
+
+        //q2
+//        String q2 = "S I B A S I S H";
+//        System.out.println("Before replace the string is:"+q2);
+//        String q2a = q2.replace(" ", "_");
+//        System.out.println("After replace the string is:"+q2a);
+
+        //q3
+//        String q3 = "Dear <|name|>, Thanks a lot";
+//        System.out.println("As at first"+ q3);
+//        String q3a = q3.replace("<|name|>","Sibasish");
+//        System.out.println("After changing"+ q3a);
+
+        //q4
+//        String q4 = "This string contains double and  triple spaces   ";
+//        System.out.println(q4.indexOf("  "));
+//        System.out.println(q4.indexOf("   "));
+//        System.out.println(q4.length());
+
+        //q5
+        String q5 = "Dear 7 Thanks for being in my life. Thank you again";
+        System.out.println("Before sequence character"+q5);
+    String q5a = "Dear 7 \n \t Thanks for being in my life.\n Thank you again";
+        System.out.println("After sequence character");
+        System.out.println(q5a);
+
+    }
+}
